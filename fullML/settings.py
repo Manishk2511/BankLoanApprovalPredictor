9ea44c99c7c91cc114c926d7590034e929c695b4
Manishk2511 <43514180+Manishk2511@users.noreply.github.com>
2020-09-08 22:02:07 +0530

    Update settings.py

--- fullML/settings.py
+++ fullML/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
